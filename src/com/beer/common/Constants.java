821d8c3f2dfa51755bfc03be0fdf0df45de8bafa
EC2 Default User <ec2-user@ip-10-100-246-196.ec2.internal>
2012-06-15 01:45:54 +0000

    switched from HSQL to MySQL drivers

--- src/com/beer/common/Constants.java
+++ src/com/beer/common/Constants.java
@@ -2,21 +2,16 @@ package com.beer.common;
 
 public class Constants {
 
-
-    public static final String USERNAME = "sa";
-    public static final String PASSWORD = "";
-    public static final String URL = "jdbc:hsqldb:hsql://127.0.0.1";
-    public static final String DRIVER = "org.hsqldb.jdbcDriver";
-
-
-
-    //KLUDGE - CHANGE THIS
 /*
-    public static final String USERNAME = "root";
-    public static final String PASSWORD = "sa";
+ *     public static final String USERNAME = "sa";
+ *         public static final String PASSWORD = "";
+ *             public static final String URL = "jdbc:hsqldb:hsql://127.0.0.1";
+ *                 public static final String DRIVER = "org.hsqldb.jdbcDriver";
+ *                 */
+
+    public static final String USERNAME = "brewuser";
+    public static final String PASSWORD = "password";
     public static final String URL = "jdbc:mysql://localhost:3306/brewery";
     public static final String DRIVER = "com.mysql.jdbc.Driver";
-
-*/
 
 }
